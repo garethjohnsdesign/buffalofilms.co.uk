357153d8822a65829069cfcda9728cd0a5d11417
Gareth Johns <garethjohns@Gareths-iMac.local>
2020-10-12 20:56:14 +0100

    Cache Bust

--- config/general.php
+++ config/general.php
@@ -48,7 +48,7 @@ return [
         'siteUrl'             => getenv('SITE_URL') ?: '@web',
 
         // Set the environmental variables
-        'staticAssetsVersion' => '1',
+        'staticAssetsVersion' => '2',
 
         'purgePendingUsersDuration' => 'P1M',
         
