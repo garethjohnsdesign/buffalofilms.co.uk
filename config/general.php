bc656369ff6269d40a2faec4d266b24410ae2ee0
Gareth Johns <garethjohns@Gareths-iMac.local>
2020-10-20 16:36:18 +0100

    Push static version

--- config/general.php
+++ config/general.php
@@ -48,7 +48,7 @@ return [
         'siteUrl'             => getenv('SITE_URL') ?: '@web',
 
         // Set the environmental variables
-        'staticAssetsVersion' => '2',
+        'staticAssetsVersion' => '3',
 
         'purgePendingUsersDuration' => 'P1M',
         
